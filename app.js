e904a43e122662f3e3c2a32a783b0fdbd69ab0bc
Wendy Hamiltonquinn <wendyhamiltonquinn@outlook.com>
2022-06-08 01:31:59 -0400

    created file

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+let armyVeteran = prompt('Did I serve in the military?');
+if (armyVeteran){
+    console.log('')
+
+ let irs = prompt('Do I have experience in federal government?');    
+ if (irs){
+
+    let degree = prompt('Do you have a in degree in Mathematics ?');
+    if (degree = ){
+
+        let nursePracititonal = prompt('Was becoming a truck driver on of my goals?');
+        if(nursePracitational){
+
+            let georgiaResident = prompt('Am I from Seattle, WA');
+            if(georgiaResident){
+
+
+} else {    
+    alert ('You are correct!')
+    console.log('')
+        if('') {
+            }
+
+
+        }
+    }
+ }
+}
+
+alert('You are incorrect!')
+
+} else {
+
+alert('Great Job! ');
+}
